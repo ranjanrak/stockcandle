2bbe58988c44402874b09fde470a8af2fb54718d
Rakesh R <ranjan01995@gmail.com>
2021-07-27 17:24:54 +0530

    feat: release 0.1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+README = open("README.md").read()
+
+setup(
+    name="stockcandle",  
+    version="0.1",
+    author="Rakesh R",
+    author_email="rrrakesh265@gmail.com",
+    description="Python library to fetch intraday data for any given EQ symbol",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ranjanrak/stockcandle.git",
+    packages=['stockcandle'],
+    install_requires=["requests"],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries"
+    ],
+)
---
+++ stockcandle/__init__.py
@@ -0,0 +1,11 @@
+"""
+@author: rakeshr
+"""
+
+"""
+Python client to fetch intraday LTP data for the day and calculate OHLC candle
+"""
+
+from stockcandle.intradayData import DataCollector
+
+__all__ = ["DataCollector"]
